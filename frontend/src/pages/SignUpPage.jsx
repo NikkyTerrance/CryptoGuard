fe20916b048f291e853c372cd2a5702d7cb09670
Nikky <nikkydocode@gmail.com>
2025-02-20 02:17:09 +0000

    2d

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useAuthStore } from '../store/useauthstore';
+import { useAuthStore } from '../store/useAuthStore';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
 import toast from 'react-hot-toast';
